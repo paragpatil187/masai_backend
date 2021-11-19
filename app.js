6355625f647341ac6f5ef58b3e1b4645e7aea4c6
paragpatil187 <91532920+paragpatil187@users.noreply.github.com>
2021-11-19 23:30:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+
+const users=require('./users.json');
+const express = require ('express');
+const app = express();
+
+app.use(express.json());
+app.get("/", ( req,res)=>{
+    
+    res.send=("Welcome to Home page");
+    
+
+});
+app.get("/users",(req,res)=>{
+    res.send(users)
+
+});
+app.post("/",(req,res)=>{
+   // console.log(req.body);
+   const newUsers=[...users,req.body];
+    res.send(newUsers);
+});
+app.patch("/:id",(req,res)=>{
+    //console.log(req.params)
+   // res.send("Patch");
+
+});
+app.delete("/:email",(req,res)=>{
+    const newUsers=users.filter ((user) => user.email !==
+req.params.email)
+res.send(newUsers)
+})
+app.listen(2346,function(){
+    console.log("listening on port 2346")
+});
